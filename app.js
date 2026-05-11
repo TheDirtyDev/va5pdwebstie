56d7781bb79e8475ff182c009d97c3b3902357a1
DirtyDev <104915841+TheDirtyDev@users.noreply.github.com>
2026-05-11 19:36:46 -0400

    Add nodemailer and increase JSON body limit

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const passport = require('passport');
 const DiscordStrategy = require('passport-discord').Strategy;
 const mysql = require('mysql2');
 const path = require('path');
+const nodemailer = require('nodemailer');
 const app = express();
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // --- CONFIGURATION ---
 const OWNER_ID = "895054825316839424"; 
