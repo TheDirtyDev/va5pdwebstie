393fddf17a4111024412fd2f06087409ac7a4ff6
DirtyDev <104915841+TheDirtyDev@users.noreply.github.com>
2026-05-12 13:53:25 -0400

    Security Fix

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ passport.deserializeUser((obj, done) => done(null, obj));
 
 passport.use(new DiscordStrategy({
     clientID: '1405718321747197972',
-    clientSecret: 'O_RV1t7mc0nlY_YNSfM1hXO0f4aLjSRJ',
+    clientSecret: process.env.DISCORD_CLIENT_SECRET,
     // Ensure this matches your Discord Developer Portal exactly
     callbackURL: process.env.NODE_ENV === 'production'
         ? 'https://va5pd.vercel.app/auth/discord/callback'
